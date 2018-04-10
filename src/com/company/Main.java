02af00bc65246dfba256a0ffbcfc4ae65bc772d3
ocean <1516080188@qq.com>
2018-04-10 16:27:26 +0800

    第一次

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import static sun.misc.Version.print;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //print(1, "hello");
+        System.out.print("hello world!");
+        System.out.print("hello world!");
+    }
+}
